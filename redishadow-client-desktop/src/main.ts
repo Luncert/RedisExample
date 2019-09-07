8c2fae6240f08458cc99516f3dc5ca92cbe8b1ef
Luncert <2725115515@qq.com>
2019-09-07 23:33:42 +0800

    initial redishadow-client-desktop

---
+++ redishadow-client-desktop/src/main.ts
@@ -0,0 +1,14 @@
+
+import {app, BrowserWindow} from "electron"
+
+app.on('ready', () => {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+})
